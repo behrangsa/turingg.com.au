063dbc4714231bfda4556374b0ea2869fdc4fd53
Behrang Saeedzadeh <commit@behrang.org>
2022-12-29 12:13:27 +0000

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,20 +21,41 @@ export default function Home() {
         </header>
 
         <div className={`${plex.className} ${styles.aboutUs}`}>
-          <h2>About us</h2>
-          <img src="/AdobeStock_211095871.jpeg" alt="" className={styles.heroImage}/>
+          <h2>
+            <img src="/noun-about-us-4149186.svg" height="64" alt=""/>
+            About us
+          </h2>
+          <img
+            src="/AdobeStock_211095871.jpeg"
+            alt=""
+            className={styles.heroImage}
+          />
           <p>
-            We are a small team of software developers based in
-            Melbourne,&nbsp;Australia. We help our clients with their cloud
+            Turingg, named after <a href="https://en.wikipedia.org/wiki/Alan_Turing">Alan Turing</a>,
+            father of theoretical computer science and artificial intelligence.
+            &mdash;is{" "}
+            <a href="https://behrang.org" className={styles.my}>
+              my
+            </a>{" "}
+            small software development company based in
+            Melbourne,&nbsp;Australia. I help my clients with their cloud
             infrastructure, backend &amp; frontend development, requirements
             gathering, &amp; solution design needs.
           </p>
           <h2>Services</h2>
           <p>
-            If you are looking to build a new website for your business, or if
-            you are looking to renovate your existing website(s), we can help
-            you with that by embracing modern &amp; sophisticated technologies such as
-            Next.js, Vercel, &amp; serverless.com.
+            If you are looking to build a new website for your business, or to
+            renovate your existing website, we can help you with that by
+            embracing agile &amp; high-end technologies such as{" "}
+            <a href="https://nextjs.org">Next.js</a>,{" "}
+            <a href="https://vercel.com">Vercel</a>, &amp;{" "}
+            <a href="https://www.serverless.com/cloud">serverless.com</a> or by
+            building and deploying to your custom private cloud infrastructure.
+          </p>
+          <h2>Contact us</h2>
+          <p>
+            For more information, please contact us at{" "}
+            <a href="mailto:contact@turingg.com.au">contact@turingg.com.au</a>.
           </p>
         </div>
       </main>
